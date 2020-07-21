cd066c68383d80454752bec0559a17b3721a53ec
unknown <larsonlearning@suddenlink.net>
2020-07-21 06:47:46 -0700

    HEART_RATE_ANALYSIS.c

--- HEART_RATE_ANALYSIS.c
+++ HEART_RATE_ANALYSIS.c
@@ -7,7 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#define TEST_FILE_NUMBER_OF_LINES 2757
+
+typedef struct node_s{//creates data type for a specific movie
+	float HR;
+	struct node_s *leftp;
+	struct node_s *rightp;
+}HR_row;
+
 int main(void)
 {
     FILE *fp;
@@ -26,23 +32,22 @@ int main(void)
 	int Running_Sum_HR=0;
 	float mean_HR =0;
 	float Total_HR = 0;
-	float stdev_HR[TEST_FILE_NUMBER_OF_LINES];
 	float sum_of_variance=0;
 	int i=0;
 	float k =0;
 	float j=0;
 	int l=0;
+	HR_row *hl=NULL, *hr, *prev, *temp;
     fp = fopen(inp_filename, "r");
 		//fq = fopen(out_filename, "w");
 	if (fp == NULL) { // Check that the input file exists
         printf("Could not open file %s", inp_filename);
         return 1;
     }
-	while (fscanf (fp, "%f,%lf", &HR_buffer[i], &Time_Buffer[i]) != EOF ){//Start of second do while. the reason is to get strings
-		//
+	
 	//Loop import 30 lines calculate 
-		//printf("%d\n",HR_buffer[i]);
-		
+	while (fscanf (fp, "%f,%lf", &HR_buffer[i], &Time_Buffer[i]) != EOF ){//Start of second do while. the reason is to get strings
+
 		//Time Conversion to minutes
 		Time[i]=((Time_Buffer[i]-Time_Buffer[0])/1000)*60+((Time_Buffer[i]-Time_Buffer[0])-(Time_Buffer[i]-Time_Buffer[0])/1000)/100+(fmod(Time_Buffer[i]-Time_Buffer[0], 100))/60;//
 		
@@ -57,20 +62,43 @@ int main(void)
 		}
 		//-Sum for Mean HR
 			Total_HR=Total_HR + HR_buffer[i];
-		//-Storage for the Standard Deviation HR
-			stdev_HR[l] = HR_buffer[i];
+			
+			
+			temp = (HR_row *)malloc(sizeof (HR_row));
+			//-Storage for the Standard Deviation HR
+			temp->HR= HR_buffer[i];
+			
+			if( hl == NULL)
+			{
+				temp->leftp=NULL;
+				temp->rightp=NULL;
+				hl=temp;
+				hr=temp;
+				prev=temp;
+				
+			}
+			else
+			{
+				temp->leftp=prev;
+				prev->rightp=temp;
+				temp->rightp=NULL;
+				hr=temp;
+				prev=temp;
+				
+			}
 		
 		i++;
 		k++;
 		l++;
 		if(i==30){i=0;}
-		
 	}
 	//calculate mean
 	mean_HR = Total_HR/k;
 	//calculate stdev
-	for(i=0;i<TEST_FILE_NUMBER_OF_LINES; i++){
-		sum_of_variance = sum_of_variance  + (stdev_HR[i]-mean_HR)*(stdev_HR[i]-mean_HR);
+	while(temp->leftp!=NULL){
+		sum_of_variance = sum_of_variance  + (temp->HR-mean_HR)*(temp->HR-mean_HR);
+		temp = temp->leftp;
+		free(temp->rightp);
 	}
 	printf("\n\n Number of HR Samples: %d\n\n Mean HR: %f bpm\n\n St Dev: %f", l,mean_HR,sqrt(sum_of_variance/k));
 	fclose(fp);//fclose(fq);
